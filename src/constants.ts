baf4c934710c7317532a13e358033c4e8e4f96e1
imirghania <imirghania@gmail.com>
2024-07-04 01:10:56 +0300

    adds pagination, scroll-to-top button

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const SITE_TITLE = "AiMirghani | Personal Website";
+export const SITE_DESCRIPTION =
+  "Articles, stories and tutorials about Data Science, Mathematics and Web Development";
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
+export const ARTICLES_PER_PAGE = 5;
+export const GITHUB_URL = "https://github.com/imirghania";
+export const TELEGRAM_URL = "https://t.me/AhmdIdris";
+export const INSTAGRAM_URL = "https://instagram.com/ahmedidris89";
+export const RSS_URL = "https://feed.miantiao.me/";
