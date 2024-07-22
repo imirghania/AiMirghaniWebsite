4f11859ec9acff955ea74ddd4ddfed05b7faefb6
imirghania <imirghania@gmail.com>
2024-07-22 21:30:20 +0300

    adds github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,16 @@ import icon from "astro-icon";
 import rehypePrettyCode from "rehype-pretty-code";
 // import theme from "/codeHighlightTheme.json?url";
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 const LIVE_URL = "https://imirghania.github.io";
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+let BASE_URL = LOCALHOST_URL;
+// When you're building your site in local or in CI, you could just set your URL manually
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
 
 const rehypePrettyCodeOptions = {
   theme: "dracula",
